370f53d4928d51e2a159e67242a6cd8ccc80fd79
John <johnkaranjairungu@gmail.com>
2023-03-17 10:27:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+    delete employee[key,value]
+    return employee
+ }
+function employee(employee, name, streetAddress='11 Broadway') {
+    employee[name]
+    employee[streetAddress]
+    return employee;
+  }
 // Write your solution in this file!
